6dfbcdd1c51a6f35e2954c2b860e83c977c09d5f
ElmF <elmforestw@proton.me>
2025-09-20 15:19:01 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,143 @@
+import { AutoRouter } from 'itty-router';
+
+interface Paste {
+	id: string;
+	content: string;
+	created_at: number;
+}
+
+function randomId(length = 4) {
+	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+	const array = new Uint8Array(length);
+	crypto.getRandomValues(array);
+	return Array.from(array, (x) => chars[x % chars.length]).join('');
+}
+
+const router = AutoRouter();
+router
+	.get(
+		'/',
+		(_, env: Env) =>
+			new Response(`PB(1)			General Commands manual			PB(1)
+
+NAME
+	pb - simple pastebin
+
+EXAMPLES
+	Upload some text:
+		$ curl -F 'c=<text>' ${env.HOSTNAME}
+
+	Upload a file:
+		$ curl -F 'c=@<file>' ${env.HOSTNAME}
+
+	Or use it in a pipe:
+		$ cmd | curl -F 'c=@-' ${env.HOSTNAME}
+
+	HTML Form:
+		Open ${new URL('/form', env.HOSTNAME).toString()} in a web browser.
+
+AUTHORS
+	ElmF <elmforestw@proton.me>
+
+COPYRIGHT
+	Copyright (c) 2025 ElmF. License MIT.
+
+SEE ALSO
+	https://github.com/ElmForestW/pb
+
+			September 2025				PB(1)`),
+	)
+	.get(
+		'/form',
+		() =>
+			new Response(
+				`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>pb - HTML Form</title>
+  </head>
+  <body>
+    <form action="/" method="post">
+      <textarea name="c" rows="10" cols="50"></textarea><br>
+      <input type="submit" value="paste">
+    </form>
+  </body>
+</html>`,
+				{
+					headers: {
+						'Content-Type': 'text/html; charset=UTF-8',
+					},
+				},
+			),
+	)
+	.post('/', async (request, env: Env) => {
+		const MAX_RETRIES = 10;
+		let k: string;
+		let attempts = 0;
+
+		// get form data
+		const data = await request.formData();
+		const input = data.get('c');
+		if (!input || (typeof input === 'string' && !input.trim())) {
+			return new Response('bad request', { status: 400 });
+		}
+
+		// read input content
+		let v: string;
+		if (input instanceof File) {
+			v = await input.text();
+		} else if (typeof input === 'string') {
+			v = input;
+		} else {
+			return new Response('unsupported input', { status: 400 });
+		}
+
+		// generate unique ID with retries
+		do {
+			k = randomId();
+			attempts++;
+		} while ((await env.PASTES.get(k)) && attempts < MAX_RETRIES);
+
+		if (attempts === MAX_RETRIES) {
+			return new Response('collision detected, is the random id range near exhaust?', { status: 500 });
+		}
+
+		// create paste object
+		const paste: Paste = {
+			id: k,
+			content: v,
+			created_at: Date.now(),
+		};
+
+		// store in KV
+		await env.PASTES.put(k, JSON.stringify(paste));
+
+		// return response without content
+		const result = { id: k, created_at: paste.created_at, url: `${env.HOSTNAME}/${k}` };
+		return new Response(JSON.stringify(result, null, 2), {
+			headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+		});
+	})
+	.get('/:id', async (request, env: Env) => {
+		// check if using JSON
+		const url = new URL(request.url);
+		const json = url.searchParams.get('json') === 'true';
+
+		// fetch paste
+		const stored = await env.PASTES.get(request.id!);
+		if (!stored) return new Response('paste not found', { status: 404 });
+
+		// return paste
+		const paste = JSON.parse(stored);
+		return !json
+			? new Response(paste.content)
+			: Response.json({
+					id: request.id,
+					content: paste.content,
+					created_at: paste.created_at,
+				});
+	});
+
+export default { ...router };
---
+++ test/index.spec.ts
@@ -0,0 +1,46 @@
+import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { describe, it, expect } from 'vitest';
+import worker from '../src/index';
+
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+describe('pb worker', () => {
+	it('root', async () => {
+		const request = new IncomingRequest('http://example.com');
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		expect(await response.text()).toMatchInlineSnapshot(`
+			"PB(1)			General Commands manual			PB(1)
+
+			NAME
+				pb - simple pastebin
+
+			EXAMPLES
+				Upload some text:
+					$ curl -F 'c=<text>' https://pb.elmforestw-db1.workers.dev
+
+				Upload a file:
+					$ curl -F 'c=@<file>' https://pb.elmforestw-db1.workers.dev
+
+				Or use it in a pipe:
+					$ cmd | curl -F 'c=@-' https://pb.elmforestw-db1.workers.dev
+
+				HTML Form:
+					Open https://pb.elmforestw-db1.workers.dev/form in a web browser.
+
+			AUTHORS
+				ElmF <elmforestw@proton.me>
+
+			COPYRIGHT
+				Copyright (c) 2025 ElmF. License MIT.
+
+			SEE ALSO
+				https://github.com/ElmForestW/pb
+
+						September 2025				PB(1)"
+		`);
+	});
+});
